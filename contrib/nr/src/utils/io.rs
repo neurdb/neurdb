30120e6da242337433ade40e01c6e8c8bab3117d
working <57171759+NLGithubWP@users.noreply.github.com>
2024-06-25 13:14:56 +0800

    Python server

---
+++ contrib/nr/src/utils/io.rs
@@ -0,0 +1,23 @@
+use reqwest::blocking::Client;
+use serde_json::json;
+use std::collections::HashMap;
+
+pub fn send_request(url: &str, task_json: &str) -> Result<String, Box<dyn Error>> {
+    // Create an HTTP client
+    let client = Client::new();
+
+    // Send the request
+    let response = client
+        .post(url)
+        .body(task_json.to_string())
+        .header("Content-Type", "application/json")
+        .send()?;
+
+    // Check the response status and get the response text
+    if response.status().is_success() {
+        let response_json: Value = response.json()?;
+        Ok(response_json.to_string())
+    } else {
+        Err(format!("Failed to send request: {}", response.status()).into())
+    }
+}
