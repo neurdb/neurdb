78f4857c4734981417cd6b5706aa3cd18a5a3ad2
working <57171759+NLGithubWP@users.noreply.github.com>
2024-06-25 13:14:56 +0800

    Python server

--- contrib/nr/src/models/mlp_clf.rs
+++ contrib/nr/src/models/mlp_clf.rs
@@ -38,7 +38,9 @@ pub fn mlp_clf(
     let task_json = json!(task_map).to_string();
 
     // Call the Python function to perform regression
-    let eva_results = run_python_function(&PY_MODULE, &task_json, "mlp_clf");
+    // let eva_results = run_python_function(&PY_MODULE, &task_json, "mlp_clf");
+    // Call the send_request function to perform the HTTP request
+    let eva_results = send_request("localhost:8090/mlp_clf", &task_json)?;
 
     // Measure the time taken for the entire process
     let _end_time = Instant::now();
--- contrib/nr/tests/utils/main.rs
+++ contrib/nr/tests/utils/main.rs
@@ -1,19 +1,18 @@
-use neurdb_extension::{parse_expr, expr_to_sql, Expr};
+use neurdb_extension::{send_request};
 
 #[test]
-fn test_parse_expr() {
-    let expr_str = "{COLUMNREF :fields (a b c)}";
-    let parsed_expr = parse_expr(expr_str);
-    if let Expr::ColumnRef { fields } = parsed_expr {
-        assert_eq!(fields, vec!["a", "b", "c"]);
-    } else {
-        panic!("Expected ColumnRef");
-    }
-}
+fn test_send_request() {
+// Step 1: Prepare task map for the Python function
+    let mut task_map = HashMap::new();
+    task_map.insert("where_cond", "where_cond");
+    task_map.insert("table", "table");
+    task_map.insert("label", "label");
+    task_map.insert("config_file", "config_file");
 
-#[test]
-fn test_expr_to_sql() {
-    let expr = Expr::Const("value".to_string());
-    let sql = expr_to_sql(&expr);
-    assert_eq!(sql, "'value'");
+    // Convert task map to JSON string
+    let task_json = json!(task_map).to_string();
+    let eva_results = send_request("localhost:8090/mlp_clf", &task_json)?;
+
+    // Prepare the response with results and metadata
+    println!(eva_results.to_string());
 }
