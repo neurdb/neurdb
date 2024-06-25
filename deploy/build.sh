c580614148d8115f8eda78ce8ed75123b6b66ab6
working <57171759+NLGithubWP@users.noreply.github.com>
2024-06-25 13:29:27 +0800

    Update headers

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status
 set -e
@@ -5,10 +6,16 @@ set -e
 # Print each command before executing it
 set -x
 
-docker rm -f neurdb_dev
+# Remove the Docker container if it exists, suppressing errors if it doesn't
+docker rm -f neurdb_dev || true
+
+# Build the Docker image
 docker build -t neurdbimg .
+
+# Run the Docker container
 docker run -d --name neurdb_dev \
     -v ~/neurdb-dev:/code/neurdb-dev \
     neurdbimg
 
+# Follow the Docker container logs
 docker logs -f neurdb_dev
