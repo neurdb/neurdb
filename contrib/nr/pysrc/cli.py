216f7390941635be1f3cfe81e04358f51a10958e
tinyAdapter <tinyadapter@outlook.com>
2024-07-17 19:16:56 +0800

    Python API package, Experiment CLI interface & Model update (Done) (#54)
    * refactor: CLI - Setup class
    
    * fix: logic issue for retrieving last created layer
    
    * refactor & feat: CLI - finetune
    
    * doc: update CLI

--- contrib/nr/pysrc/cli.py
+++ contrib/nr/pysrc/cli.py
@@ -1,6 +1,7 @@
 import argparse
 import os
-from typing import List, NewType, Tuple
+from typing import List, Optional, Tuple
+from io import BytesIO
 
 import numpy as np
 import torch
@@ -11,12 +12,9 @@ from apps import build_model
 from connection import DatabaseModelHandler, NeurDBModelHandler
 from utils.dataset import libsvm_dataloader, build_inference_loader
 
-# from utils.io import save_model_weight, load_model_weight
-from io import BytesIO
-
 # from cache.model_cache import ModelCache
 
-Error = NewType("Error", str)
+Error = Optional[str]
 
 # Load config and initialize once
 # model_cache = ModelCache()
@@ -26,52 +24,32 @@ MODEL_HANDLER = NeurDBModelHandler
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
-def _do_train(
-    model_name: str,
-    batch_size: int,
-    libsvm_file: bytes,
-    args: argparse.Namespace,
-    db: MODEL_HANDLER,
-) -> Tuple[int, Error]:
-    try:
-        file_obj = BytesIO(libsvm_file)
+class Setup:
+    def __init__(
+        self,
+        model_name: str,
+        dataset_file_path: str,
+        args: argparse.Namespace,
+        db: MODEL_HANDLER,
+    ) -> None:
+        self._model_name = model_name
+        self._dataset_file_path = dataset_file_path
+        self._args = args
+        self._db = db
 
-        train_loader, val_loader, test_loader, nfields, nfeat = libsvm_dataloader(
-            batch_size, args.data_loader_worker, file_obj
-        )
+        self._dataset_file = self._load_dataset()
 
-        builder = build_model(model_name, args)
-        builder.model_dimension = (nfeat, nfields)
-        builder.train(train_loader, val_loader, test_loader)
+    def _load_dataset(self) -> bytes:
+        with open(self._dataset_file_path, "rb") as f:
+            return f.read()
 
-        model_id = db.insert_model(builder.model)
-
-        return model_id, None
-
-    except Exception:
-        return -1, str(traceback.format_exc())
-
-
-def _do_inference(
-    model_name: str,
-    model_id: int,
-    libsvm_file: bytes,
-    args: argparse.Namespace,
-    db: MODEL_HANDLER,
-) -> Tuple[List[np.ndarray], Error]:
-    try:
-        file_obj = BytesIO(libsvm_file)
-
-        inference_loader, nfields, nfeat = build_inference_loader(
-            args.data_loader_worker, file_obj
-        )
-
-        builder = build_model(model_name, args)
-
-        # TODO: I think we should change NeurDB APIs here. It should infer the 
-        # model args automatically, or store the args into DB (model table), 
+    def _make_model_args(self, nfields: int, nfeat: int) -> dict:
+        # TODO: I think we should change NeurDB APIs here. It should infer the
+        # model args automatically, or store the args into DB (model table),
         # instead of manually input
-        model_args = {
+        args = self._args
+
+        return {
             "model": {
                 "nfield": nfields,
                 "nfeat": nfeat,
@@ -116,67 +94,143 @@ def _do_inference(
                 "ensemble_layer": {"in_features": 2, "out_features": 1},
             },
         }
+
+    def train(self, batch_size: int) -> Tuple[int, Error]:
         try:
-            builder.model = db.get_model(model_id, model_args).to(DEVICE)
-        except FileNotFoundError:
-            return [], f"model {model_name} not trained yet"
+            train_loader, val_loader, test_loader, nfields, nfeat = libsvm_dataloader(
+                batch_size,
+                self._args.data_loader_worker,
+                BytesIO(self._dataset_file),
+            )
 
-        # check if test data matching model dimension
-        # model_nfeat, model_nfields = builder.model_dimension
-        # if nfields > model_nfields or nfeat > model_nfeat:
-        #     return (
-        #         [],
-        #         f"model {model_name} trained with nfields = {model_nfields} and nfeat = {model_nfeat}. "
-        #         f"cannot handle input with nfields = {nfields} and nfeat = {nfeat}.",
-        #     )
+            builder = build_model(self._model_name, self._args)
+            builder.model_dimension = (nfeat, nfields)
+            builder.train(train_loader, val_loader, test_loader)
 
-        infer_res = builder.inference(inference_loader)
-        return infer_res, None
+            model_id = self._db.insert_model(builder.model)
 
-    except Exception:
-        return [], str(traceback.format_exc())
+            return model_id, None
+
+        except Exception:
+            return -1, str(traceback.format_exc())
+        
+    def finetune(self, model_id: int, batch_size: int, start_layer_id: int) -> Tuple[int, Error]:
+        try:
+            train_loader, val_loader, test_loader, nfields, nfeat = libsvm_dataloader(
+                batch_size,
+                self._args.data_loader_worker,
+                BytesIO(self._dataset_file),
+            )
+
+            try:
+                builder = build_model(model_name, self._args)
+                model_args = self._make_model_args(nfields, nfeat)
+                model_storage = self._db.get_model(model_id, model_args)
+                model = model_storage.to_model()
+            except FileNotFoundError:
+                return -1, f"model {model_name} not trained yet"
+            
+            for i, (_, layer) in enumerate(model.named_children()):
+                if i < start_layer_id:
+                    layer.requires_grad_(False)
+            
+                builder.model = model.to(DEVICE)
+            
+            builder.model_dimension = (nfeat, nfields)
+            builder.train(train_loader, val_loader, test_loader)
+
+            model_id = self._db.update_layers(model_id, model_storage, start_layer_id)
+
+            return model_id, None
+
+        except Exception:
+            return -1, str(traceback.format_exc())
+
+    def inference(self, model_id: int) -> Tuple[List[np.ndarray], Error]:
+        try:
+            inference_loader, nfields, nfeat = build_inference_loader(
+                self._args.data_loader_worker, BytesIO(self._dataset_file)
+            )
+
+            builder = build_model(model_name, self._args)
+            model_args = self._make_model_args(nfields, nfeat)
+            try:
+                builder.model = self._db.get_model(model_id, model_args).to_model().to(DEVICE)
+            except FileNotFoundError:
+                return [], f"model {model_name} not trained yet"
+
+            # check if test data matching model dimension
+            # model_nfeat, model_nfields = builder.model_dimension
+            # if nfields > model_nfields or nfeat > model_nfeat:
+            #     return (
+            #         [],
+            #         f"model {model_name} trained with nfields = {model_nfields} and nfeat = {model_nfeat}. "
+            #         f"cannot handle input with nfields = {nfields} and nfeat = {nfeat}.",
+            #     )
+
+            infer_res = builder.inference(inference_loader)
+            return infer_res, None
+
+        except Exception:
+            return [], str(traceback.format_exc())
 
 
 def train(
-    file_path: str,
-    batch_size: int,
     model_name: str,
+    file_path: str,
     args: argparse.Namespace,
     db: MODEL_HANDLER,
+    batch_size: int,
 ) -> int:
-    with open(file_path, "rb") as f:
-        libsvm_file = f.read()
+    s = Setup(model_name, file_path, args, db)
 
-    model_id, err = _do_train(model_name, batch_size, libsvm_file, args, db)
+    model_id, err = s.train(batch_size)
     if err is not None:
         logger.error(f"train failed with error: {err}")
-        return
+        return -1
 
     print(f"train done. model_id: {model_id}")
 
     return model_id
 
 
-def inference(
-    file_path: str,
+def finetune(
     model_name: str,
-    model_id: int,
+    file_path: str,
     args: argparse.Namespace,
     db: MODEL_HANDLER,
-) -> List[np.ndarray]:
-    with open(file_path, "rb") as f:
-        libsvm_file = f.read()
+    model_id: int,
+    batch_size: int,
+) -> int:
+    s = Setup(model_name, file_path, args, db)
 
-    response, err = _do_inference(model_name, model_id, libsvm_file, args, db)
+    model_id, err = s.finetune(model_id, batch_size, start_layer_id=5)
+    if err is not None:
+        logger.error(f"train failed with error: {err}")
+        return -1
+
+    print(f"finetune done. model_id: {model_id}")
+
+    return model_id
+
+def inference(
+    model_name: str,
+    file_path: str,
+    args: argparse.Namespace,
+    db: MODEL_HANDLER,
+    model_id: int,
+) -> List[np.ndarray]:
+    s = Setup(model_name, file_path, args, db)
+
+    response, err = s.inference(model_id)
     if err is not None:
         logger.error(f"inference failed with error: {err}")
-        return
+        return []
 
     logger.debug(f"inference done. response[0,:100]:")
     logger.debug(response[0][:100] if len(response[0]) >= 100 else response[0])
 
     return response
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -202,7 +256,6 @@ if __name__ == "__main__":
             # "password": config_args.db_password,
         }
     )
-    db.connect()
     logger.debug(f"connected to DB")
 
     file_path = os.path.abspath("../../../dataset/frappe/test.libsvm")
@@ -210,9 +263,14 @@ if __name__ == "__main__":
     logger.info(f"file_path={file_path}, model_name={model_name}")
 
     if not args.train:
+        logger.info("mode: inference")
         model_id = args.model_id
+    elif db.has_model(args.model_id):
+        logger.info("model exists. mode: finetune")
+        model_id = finetune(model_name, file_path, config_args, db, args.model_id, 32)
     else:
-        model_id = train(file_path, 32, model_name, config_args, db)
+        logger.info("model does not exist. mode: train")
+        model_id = train(model_name, file_path, config_args, db, 32)
 
     if args.inference:
-        inference(file_path, model_name, model_id, config_args, db)
+        inference(model_name, file_path, config_args, db, model_id)
