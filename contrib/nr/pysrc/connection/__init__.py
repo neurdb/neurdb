d3704130d25a491a5350eedcc3056ff310218784
working <57171759+NLGithubWP@users.noreply.github.com>
2024-07-22 22:28:35 +0800

    Clear the unused packages

--- contrib/nr/pysrc/connection/__init__.py
+++ contrib/nr/pysrc/connection/__init__.py
@@ -1,2 +1,1 @@
-from .pg_connect import DatabaseModelHandler
 from .nr import NeurDBModelHandler
--- contrib/nr/pysrc/models/mlp_clf/builder.py
+++ contrib/nr/pysrc/models/mlp_clf/builder.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from sklearn.metrics import accuracy_score
-from connection.pg_connect import LoadedDataset
 from models.mlp_clf.model.model import MLP
 from models.base.builder import BuilderBase
 
