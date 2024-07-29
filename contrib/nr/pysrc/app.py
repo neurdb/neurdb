8c510f42893bd9e95188153fb28c87c2d9247c44
working <57171759+NLGithubWP@users.noreply.github.com>
2024-07-29 11:35:50 +0800

    Update import data

--- contrib/nr/pysrc/app.py
+++ contrib/nr/pysrc/app.py
@@ -2,13 +2,12 @@ from logger.logger import configure_logging
 from flask import Flask
 from shared_config.config import parse_config_arguments
 from app.routes import train_bp, inference_bp, finetune_bp
-from app.socketio.data_socketio import NRDataManager
 from app.routes.context import before_request_func, after_request_func
-from app.socketio.data_socketio import socketio
+from app.socketio.data_socketio import socketio, NRDataManager
 from cache import ContextStates, DataCache, LibSvmDataDispatcher
 from connection import NeurDBModelHandler
 from neurdb.logger import configure_logging as api_configure_logging
-from flask import request, jsonify, current_app, g
+from flask import jsonify
 
 # configure_logging("./app.log")
 configure_logging(None)
@@ -19,16 +18,15 @@ app = Flask(__name__)
 config_path = "./config.ini"
 config_args = parse_config_arguments(config_path)
 
-NEURDB_CONNECTOR = None
-#     NeurDBModelHandler(
-#     {
-#         "db_name": config_args.db_name,
-#         "db_user": config_args.db_user,
-#         "db_host": config_args.db_host,
-#         "db_port": config_args.db_port,
-#         # "password": config_args.db_password,
-#     }
-# )
+NEURDB_CONNECTOR = NeurDBModelHandler(
+    {
+        "db_name": config_args.db_name,
+        "db_user": config_args.db_user,
+        "db_host": config_args.db_host,
+        "db_port": config_args.db_port,
+        # "password": config_args.db_password,
+    }
+)
 
 # shared global contexts among tasks.
 with app.app_context():
