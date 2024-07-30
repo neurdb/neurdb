819daf3627a40bb066dad65caf6c1b94d0f6f633
siqi <siqxiang@gmail.com>
2024-07-31 02:57:40 +0800

    [fix] move SPI_execute up

--- contrib/nr_preprocessing/src/interface.c
+++ contrib/nr_preprocessing/src/interface.c
@@ -130,11 +130,11 @@ Datum nr_inference(PG_FUNCTION_ARGS) {
 
     while (true) {
         current_batch++;
+        SPI_execute(query.data, false, batch_size);
         if (SPI_processed == 0 || current_batch > n_batches) {
             elog(INFO, "Inference completed");
             break; // no more rows to fetch, break the loop
         }
-        SPI_execute(query.data, false, batch_size);
         resetStringInfo(&libsvm_data);
         resetStringInfo(&row_data);
         // get the query result
