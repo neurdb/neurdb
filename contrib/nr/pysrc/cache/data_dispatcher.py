655746e47e273caf7dc99fee4cdd5aa6e649b95f
working <57171759+NLGithubWP@users.noreply.github.com>
2024-07-31 02:43:59 +0800

    Move the set ti before

--- contrib/nr/pysrc/cache/data_dispatcher.py
+++ contrib/nr/pysrc/cache/data_dispatcher.py
@@ -124,10 +124,9 @@ class LibSvmDataDispatcher:
 
         # Add the processed data to the cache
         if self.data_cache.add(batch_data):
+            # Notify that new data is available
+            self.full_event.set()
             logger.debug(f"[LibSvmDataDispatcher]: Data added, cur length = {self.data_cache.current_len()}")
         else:
             logger.debug(f"[LibSvmDataDispatcher]: stopoing dispacher threads,no data to add after waiting for 10 mins")
             self.stop()
-
-        # Notify that new data is available
-        self.full_event.set()
