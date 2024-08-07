91f228f0d9688e6e9d78008aa29216c34e1b98f9
Panpan <panh@u.nus.edu>
2024-08-07 15:41:11 +0800

    Support learned CC

--- dbengine/scripts/cleanup.sh
+++ dbengine/scripts/cleanup.sh
@@ -7,6 +7,8 @@ echo "max_connections = 1000" >> $PGDATA/postgresql.conf
 echo "shared_buffers = '4GB'" >> $PGDATA/postgresql.conf
 echo "fsync = 'off'" >> $PGDATA/postgresql.conf
 echo "full_page_writes = 'on'" >> $PGDATA/postgresql.conf
+echo "max_locks_per_transaction = 128" >> $PGDATA/postgresql.conf
+#echo "log_min_messages = debug5" >> $PGDATA/postgresql.conf
 
 pg_ctl start -D $PGDATA
 psql -d postgres -c 'create database ycsb';
