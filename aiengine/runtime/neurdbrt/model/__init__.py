94c1267355aca339685b244bcbe391a877566750
tinyAdapter <tinyadapter@outlook.com>
2024-11-07 12:51:40 +0800

    fix: TRAIN ON does not use nr_model_name config

--- aiengine/runtime/neurdbrt/model/__init__.py
+++ aiengine/runtime/neurdbrt/model/__init__.py
@@ -1,9 +1,11 @@
 from .armnet import *
 from .base import *
 from .mlp_clf import *
-
+from ..log import logger
 
 def build_model(model_name: str, config_args) -> BuilderBase:
+    logger.info("building model", model_name=model_name)
+    
     model = None
     if model_name == "armnet":
         model = ARMNetModelBuilder(config_args)
