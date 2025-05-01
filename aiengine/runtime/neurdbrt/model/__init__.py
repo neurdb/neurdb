009bee2edd95be5ce3693615b6a64ec23e826048
tinyAdapter <tinyadapter@outlook.com>
2025-05-01 09:01:25 +0000

    feat(aiengine): model pool, dynamic model registration & lifecycle hooks
    As a first step, neurdb_on_start() hook is implemented to support registering ARM-Net to the model pool.

--- aiengine/runtime/neurdbrt/model/__init__.py
+++ aiengine/runtime/neurdbrt/model/__init__.py
@@ -1,15 +1,18 @@
+from typing import Type
 from ..log import logger
-from .armnet import *
 from .base import *
-from .mlp_clf import *
 
+_name_builder_map = {}
+
+def register_model(model_name: str, builder: Type[BuilderBase]):
+    logger.info("registering model", model_name=model_name)
+    _name_builder_map[model_name] = builder
 
 def build_model(model_name: str, config_args) -> BuilderBase:
     logger.info("building model", model_name=model_name)
 
-    model = None
-    if model_name == "armnet":
-        model = ARMNetModelBuilder(config_args)
-    # if model_name == "mlp":
-    # model = MLPBuilder(config_args)
-    return model
+    builder = _name_builder_map.get(model_name)
+    if not builder:
+        raise ValueError(f"model {model_name} not found")
+    
+    return builder(config_args)
