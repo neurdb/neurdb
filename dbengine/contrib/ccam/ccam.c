4b1b4298d2fc4af7c8db568ca1d8a404ef1b8622
Pan pan <panh@u.nus.edu>
2025-05-06 08:25:18 +0000

    feat(ccam): add access method as extension.

---
+++ dbengine/contrib/ccam/ccam.c
@@ -0,0 +1,35 @@
+#include "postgres.h"
+#include "fmgr.h"
+#include "access/tableam.h"
+// #include "utils/guc.h"
+
+PG_MODULE_MAGIC;
+
+void _PG_init(void);
+void _PG_fini(void);
+
+Datum occam_tableam_handler(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(occam_tableam_handler);
+
+static const TableAmRoutine occam_methods = {
+    .type = T_TableAmRoutine,
+    .slot_callbacks = table_slot_callbacks, // reuse heap slot for now
+    .scan_begin = NULL,                      // you'll implement this
+    .scan_getnextslot = NULL,
+    .tuple_insert = NULL,
+    .tuple_update = NULL,
+    .tuple_delete = NULL,
+    // other methods as needed
+};
+
+Datum
+occam_tableam_handler(PG_FUNCTION_ARGS)
+{
+    PG_RETURN_POINTER(&occam_methods);
+}
+
+void
+_PG_init(void)
+{
+    // Optional: GUCs here
+}
