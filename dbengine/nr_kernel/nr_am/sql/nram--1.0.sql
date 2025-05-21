705da991a5afba7588381af1e27bad7319ec0810
Pan pan <panh@u.nus.edu>
2025-05-21 04:19:51 +0000

    fix(ext): some extension so link files only get refreshed with new db connection. Refine the scripts to fix this.

--- dbengine/nr_kernel/nr_am/sql/nram--1.0.sql
+++ dbengine/nr_kernel/nr_am/sql/nram--1.0.sql
@@ -1,6 +1,7 @@
 -- nram--1.0.sql
-CREATE OR REPLACE FUNCTION nram_tableam_handler(internal)
-RETURNS table_am_handler AS 'nram', 'nram_tableam_handler'
+CREATE FUNCTION nram_tableam_handler(internal)
+RETURNS table_am_handler
+AS 'nram', 'nram_tableam_handler'
 LANGUAGE C STRICT;
 
 CREATE ACCESS METHOD nram TYPE TABLE HANDLER nram_tableam_handler;
--- dbengine/nr_kernel/nr_am/src/nram.h
+++ dbengine/nr_kernel/nr_am/src/nram.h
@@ -1,6 +1,15 @@
 #ifndef NRAM_H
 #define NRAM_H
 
-// TODO: add the necessary includes and definitions for NRAM.
+#include "kv_access/kv.h"
+#include "kv_storage/rocksengine.h"
+#include "test/kv_test.h"
+
+typedef struct NRAMState {
+    KVEngine *engine;       // Pointer to the RocksDB backend
+    int *key_attrs;         // Array of key attribute numbers
+    int nkeys;              // Number of primary key attributes
+} NRAMState;
+
 
 #endif //NRAM_H
