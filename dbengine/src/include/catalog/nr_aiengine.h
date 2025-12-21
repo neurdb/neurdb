30499a0a3406d3a8d8025a468a6635f0e0e82f96
tinyAdapter <tinyadapter@outlook.com>
2025-12-21 09:25:38 +0000

    feat(catalog)!: add nr_aiengine system catalog
    BREAKING CHANGES: Need re-initialize db with `initdb`

---
+++ dbengine/src/include/catalog/nr_aiengine.h
@@ -0,0 +1,22 @@
+#ifndef NR_AIENGINE_H
+#define NR_AIENGINE_H
+
+#include "catalog/genbki.h"
+#include "catalog/nr_aiengine_d.h"
+
+CATALOG(nr_aiengine,9600,NrAiengineRelationId)
+{
+	Oid			oid;
+	NameData	aiename;
+	int32		aieport BKI_DEFAULT(0);
+	Oid			aieconn BKI_DEFAULT(0);
+#ifdef CATALOG_VARLEN			/* variable-length fields start here */
+	text		aieaddr BKI_FORCE_NOT_NULL; /* aiengine address */
+#endif
+} FormData_nr_aiengine;
+
+typedef FormData_nr_aiengine * Form_nr_aiengine;
+
+/* Indexes */
+DECLARE_UNIQUE_INDEX(nr_aiengine_oid_index, 9601, NrAiengineOidIndexId, on nr_aiengine using btree(oid oid_ops));
+#endif
